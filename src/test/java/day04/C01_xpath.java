1f9887515a4c137990e8de14fc2b5c15e643f593
veyselDursun <veysdursun@yahoo.com>
2023-06-20 00:24:07 +0200

    day05_C01

---
+++ src/test/java/day04/C01_xpath.java
@@ -0,0 +1,49 @@
+package day04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C01_xpath {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("Webdriver.chrome.driver","src/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    // 1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+    // 2- Add Element butonuna basin
+        driver.findElement(By.xpath("//button[@onclick='addElement()']")).click();
+
+    // 3- Delete butonu’nun gorunur oldugunu test edin
+        WebElement deletButon = driver.findElement(By.xpath("//button[@onclick='deleteElement()']"));
+        if (deletButon.isDisplayed()){
+            System.out.println("Delete butonu gorunme testi PASSED");
+        }else {
+            System.out.println("Delete butonu gorunme testi FAILED");
+        }
+        Thread.sleep(2000);
+    // 4- Delete tusuna basin
+        deletButon.click();
+
+    // 5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
+        WebElement addRemoveElement = driver.findElement(By.xpath("//h3"));
+        //WebElement addRemoveElement = driver.findElement(By.tagName("h3"));
+        if (addRemoveElement.isDisplayed()){
+            System.out.println("Add Remove testi PASSED");
+        }else {
+            System.out.println("Add Remove testi FAILED");
+        }
+
+        Thread.sleep(2000);
+        driver.close();
+
+    }
+
+}
